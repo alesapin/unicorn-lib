e434b2374b75ff0463829dc76bbf62a781cc3daf
alesapin <alesapin@gmail.com>
2019-01-03 19:09:35 +0300

    Add simple build

---
+++ contrib/pcre2-10.32/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/alesap/code/cpp/unicorn-lib/contrib/pcre2-10.32
+# Build directory: /home/alesap/code/cpp/unicorn-lib/contrib/pcre2-10.32
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(pcre2_test "sh" "/home/alesap/code/cpp/unicorn-lib/contrib/pcre2-10.32/pcre2_test.sh")
---
+++ contrib/pcre2-10.32/pcre2_test.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+# This is a generated file.
+. /home/alesap/code/cpp/unicorn-lib/contrib/pcre2-10.32/RunTest
+if test "$?" != "0"; then exit 1; fi
+# End
